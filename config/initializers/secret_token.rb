84eec6ccf493335de69c6ffc6ff5ff2803461c3a
Manojs <manojs.nitt@gmail.com>
2012-01-08 10:13:08 +0530

    GreenDay application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenDay::Application.config.secret_token = '55c64a4b6c00522677fa850b72e4ec689d21eaf2b4bf64f231ac09a75f679857cae2c1a8eddbd980b03392fe934e8f9b0f4ce020bba82ae95e2139a9c23b33cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GreenDay::Application.config.session_store :cookie_store, key: '_GreenDay_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GreenDay::Application.config.session_store :active_record_store
